2faf343eb27c3219dcf5cbfe00a877def42c6859
NgaNguyenDuy <kyo1508@gmail.com>
2016-04-22 14:45:44 +0700

    added: Main config file to load all configurations

---
+++ src/init.el
@@ -0,0 +1,26 @@
+;;
+;; Global variable
+;;
+(defvar *cfg-dir* (file-name-directory (or (buffer-file-name) load-file-name))
+  "Return config dirertory.")
+
+;;
+;; Config helpers
+;;
+(defun ~get-cfg (&rest paths)
+  "Returns path to a config file or directory."
+  (apply 'concat *cfg-dir* paths))
+
+;; Load config function
+(defun load-f (&rest p)
+  "Return full file path."
+  (apply 'concat *cfg-dir* p))
+
+
+(require 'e:funcs (load-f "funcs.el"))
+(require 'e:list-pkg (load-f "tspyimt-pkg.el"))
+(require 'e:load-pkg (load-f "load-pkg.el"))
+;; (require 'e:config-pkg (load-f "config-pkg.el"))
+(require 'e:settings (load-f "settings.el"))
+(require 'e:env (load-f "env.el"))
+(require 'e:keybindings (load-f "keybindings.el"))
